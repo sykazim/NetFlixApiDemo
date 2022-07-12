7659055bf0c0dedc68061cc3ffd8ee6ae58c0f52
sykazim <109132092+sykazim@users.noreply.github.com>
2022-07-12 12:52:57 +0530

    initial  netflix api commit

---
+++ HashedInHuEdgeApiApplication.java
@@ -0,0 +1,13 @@
+package com.netflix.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashedInHuEdgeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HashedInHuEdgeApiApplication.class, args);
+	}
+
+}
